e15afa5b8122ac44715f38e9369d58d2114a5575
bhlezzdmplyy <felicitybhlezzgarbin@gmail.com>
2024-09-10 11:40:39 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+// Create two variables to store favorite numbers
+let firstFavNum = 7;
+let secondFavNum = 3;
+
+// Perform arithmetic operations and log the results
+
+// Addition
+console.log("Addition:", firstFavNum + secondFavNum);
+
+// Subtraction
+console.log("Subtraction:", firstFavNum - secondFavNum);
+
+// Multiplication
+console.log("Multiplication:", firstFavNum * secondFavNum);
+
+// Division
+console.log("Division:", firstFavNum / secondFavNum);
+
+// Modulus (Remainder)
+console.log("Modulus:", firstFavNum % secondFavNum);
+
+// Exponentiation (Power)
+console.log("Exponentiation (Power):", firstFavNum ** secondFavNum);
